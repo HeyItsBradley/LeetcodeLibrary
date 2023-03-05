bcd222e02f2feec0aad636c0544695e0e83d7aa0
HeyItsBradley <bradleyrodriguez77@gmail.com>
2023-03-04 18:22:22 -0700

    added removeDuplicates problem the the index.js file

--- index.js
+++ index.js
@@ -15,10 +15,9 @@ var twoSum = function (nums, target) {
 
 twoSum([2, 7, 11, 15], 9);
 
-
 //Palindrome Number
 
-//How it works: Check to see if given number is palendrone. First make variable to call it num of what the function is recieving. Then convert that number into a string so we can manipulate it. Then split the string at each character so it is now an array with each character at a new index. Then use the array method of reverse to reverse the order of the array. Then we rejoin the array into a string with nothing between them. Then check to see if the string variable is equal to the reverse variable called joined. If yes return true, if not return false. 
+//How it works: Check to see if given number is palendrone. First make variable to call it num of what the function is recieving. Then convert that number into a string so we can manipulate it. Then split the string at each character so it is now an array with each character at a new index. Then use the array method of reverse to reverse the order of the array. Then we rejoin the array into a string with nothing between them. Then check to see if the string variable is equal to the reverse variable called joined. If yes return true, if not return false.
 var isPalindrome = function (x) {
   let num = x;
   let string = num.toString();
@@ -33,3 +32,18 @@ var isPalindrome = function (x) {
 };
 
 isPalindrome(121);
+
+//Remove Duplicates from sorted Array
+
+//How it works: Run a loop through the array of numbers. then check each index, if that current index is uqual to the index plus one, then splice that index(the plus one index) from the array using array.splice. Then suptract one from the loop interator in this case i--. This is done so if there are lets say 4 of one number, it checks the array over and over again until there is only one of that number in the array.
+
+var removeDuplicates = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      nums.splice(i + 1, 1);
+      i--;
+    }
+  }
+};
+
+removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
